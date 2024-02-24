46e9bfe449cf945546da0fefbc8abb41e36a415a
John Gietzen <john@gietzen.us>
2024-02-24 15:11:40 -0800

    Two-display demo for "CenterScreen"

--- RenderLoop/Demo/CenterScreen.cs
+++ RenderLoop/Demo/CenterScreen.cs
@@ -8,42 +8,58 @@
 
     internal class CenterScreen : Cube
     {
-        public CenterScreen(Display display)
-            : base(display)
+        private readonly Display display2;
+
+        public CenterScreen(Display display1, Display display2)
+            : base(display1)
         {
+            this.display2 = display2;
+        }
+
+        protected override void Initialize()
+        {
+            var location = this.display.Location;
+            location.X += 100;
+            location.Y += 100;
+            this.display2.StartPosition = FormStartPosition.Manual;
+            this.display2.Location = location;
+            this.display2.Show(this.display);
         }
 
         protected override void DrawScene(AppState state, TimeSpan elapsed)
         {
-            this.display.PaintFrame(elapsed, (Graphics g, Bitmap buffer, float[,] depthBuffer) =>
+            foreach (var display in new[] { this.display, this.display2 }.Where(d => !d.IsDisposed))
             {
-                var screen = Screen.FromControl(this.display);
-                this.Camera.Width = screen.Bounds.Width;
-                this.Camera.Height = screen.Bounds.Height;
-
-                var topLeft = this.display.PointToScreen(Point.Empty);
-
-                var transformed = Array.ConvertAll(Vertices, v =>
+                display.PaintFrame(elapsed, (Graphics g, Bitmap buffer, float[,] depthBuffer) =>
                 {
-                    var p = this.Camera.TransformToScreenSpace(v);
-                    p.X -= topLeft.X;
-                    p.Y -= topLeft.Y;
-                    return p;
+                    var screen = Screen.FromControl(display);
+                    this.Camera.Width = screen.Bounds.Width;
+                    this.Camera.Height = screen.Bounds.Height;
+
+                    var topLeft = display.PointToScreen(Point.Empty);
+
+                    var transformed = Array.ConvertAll(Vertices, v =>
+                    {
+                        var p = this.Camera.TransformToScreenSpace(v);
+                        p.X -= topLeft.X;
+                        p.Y -= topLeft.Y;
+                        return p;
+                    });
+
+                    foreach (var face in Shapes)
+                    {
+                        var indices = face.Select((i, j) => (index: i, uv: UV[j])).ToArray();
+                        Display.DrawStrip(indices, i => transformed[i.index], (v, vertices) =>
+                            Display.FillTriangle(buffer, depthBuffer, vertices, BackfaceCulling.Cull, perspective =>
+                            {
+                                var uv = Display.MapCoordinates(perspective, [v[0].uv, v[1].uv, v[2].uv]);
+                                return ((int)(uv.X * 4) + (int)(uv.Y * 4)) % 2 == 0
+                                    ? Color.White
+                                    : Color.Gray;
+                            }));
+                    }
                 });
-
-                foreach (var face in Shapes)
-                {
-                    var indices = face.Select((i, j) => (index: i, uv: UV[j])).ToArray();
-                    Display.DrawStrip(indices, i => transformed[i.index], (v, vertices) =>
-                        Display.FillTriangle(buffer, depthBuffer, vertices, BackfaceCulling.Cull, perspective =>
-                        {
-                            var uv = Display.MapCoordinates(perspective, [v[0].uv, v[1].uv, v[2].uv]);
-                            return ((int)(uv.X * 4) + (int)(uv.Y * 4)) % 2 == 0
-                                ? Color.White
-                                : Color.Gray;
-                        }));
-                }
-            });
+            }
         }
     }
 }
