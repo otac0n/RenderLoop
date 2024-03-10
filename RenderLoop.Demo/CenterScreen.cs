2664ec55c24e409b40c82ffba417555ee62ea54f
John Gietzen <john@gietzen.us>
2024-03-09 22:04:51 -0800

    Style fixes for software renderer demo.

--- RenderLoop.Demo/CenterScreen.cs
+++ RenderLoop.Demo/CenterScreen.cs
@@ -5,33 +5,34 @@ namespace RenderLoop.Demo
     using System;
     using System.Drawing;
     using System.Linq;
-    using System.Numerics;
     using System.Windows.Forms;
     using RenderLoop.SoftwareRenderer;
+    using Fragment = (System.Numerics.Vector4 Position, System.Numerics.Vector2 UV);
+    using Vertex = (System.Numerics.Vector3 Position, System.Numerics.Vector2 UV);
 
     internal class CenterScreen : CubeSW
     {
         private readonly Display display2;
 
-        private readonly DynamicDraw.Shader<Point, (Vector3 position, Vector2 uv), (Vector4 position, Vector2 uv)> shader;
+        private readonly DynamicDraw.Shader<Point, Vertex, Fragment> shader;
 
         public CenterScreen(CooperativeIdleApplicationContext context)
             : base(context)
         {
             this.display2 = context.CreateDisplay();
 
-            this.shader = DynamicDraw.MakeShader<Point, (Vector3 position, Vector2 uv), (Vector4 position, Vector2 uv)>(
+            this.shader = DynamicDraw.MakeShader<Point, Vertex, Fragment>(
                 Point.Empty,
                 (p, x) =>
                 {
-                    var pos = this.Camera.TransformToScreenSpace(x.position);
+                    var pos = this.Camera.TransformToScreenSpace(x.Position);
                     pos.X -= p.X;
                     pos.Y -= p.Y;
-                    return (pos, x.uv);
+                    return (pos, x.UV);
                 },
-                x => x.position,
-                DynamicDraw.MakeFragmentShader<(Vector4 position, Vector2 uv)>(
-                    x => x.uv,
+                x => x.Position,
+                DynamicDraw.MakeFragmentShader<Fragment>(
+                    x => x.UV,
                     uv => ((int)(uv.X * 4) + (int)(uv.Y * 4)) % 2 == 0
                         ? Color.White
                         : Color.Gray));
@@ -61,7 +62,7 @@ namespace RenderLoop.Demo
 
                     this.shader.Data = display.PointToScreen(Point.Empty);
 
-                    foreach (var face in shapes)
+                    foreach (var face in this.shapes)
                     {
                         DynamicDraw.DrawStrip(buffer, depthBuffer, face, BackfaceCulling.CullCounterClockwise, this.shader);
                     }
--- RenderLoop.Demo/CubeSW.cs
+++ RenderLoop.Demo/CubeSW.cs
@@ -5,28 +5,29 @@ namespace RenderLoop.Demo
     using System;
     using System.Drawing;
     using System.Linq;
-    using System.Numerics;
     using RenderLoop.SoftwareRenderer;
     using static Geometry;
+    using Fragment = (System.Numerics.Vector4 Position, System.Numerics.Vector2 UV);
+    using Vertex = (System.Numerics.Vector3 Position, System.Numerics.Vector2 UV);
 
     internal class CubeSW : CameraSpinner
     {
         protected readonly Display display;
 
-        protected readonly (Vector3 position, Vector2 uv)[][] shapes = Array.ConvertAll(Shapes, shape => shape.Select((i, j) => (Vertices[i], UV[j])).ToArray());
+        protected readonly Vertex[][] shapes = Array.ConvertAll(Shapes, shape => shape.Select((i, j) => (Vertices[i], UV[j])).ToArray());
 
-        private readonly DynamicDraw.Shader<(Vector3 position, Vector2 uv), (Vector4 position, Vector2 uv)> shader;
+        private readonly DynamicDraw.Shader<Vertex, Fragment> shader;
 
         public CubeSW(CooperativeIdleApplicationContext context)
             : base(context)
         {
             this.display = context.CreateDisplay();
 
-            this.shader = DynamicDraw.MakeShader<(Vector3 position, Vector2 uv), (Vector4 position, Vector2 uv)>(
-                x => (this.Camera.TransformToScreenSpace(x.position), x.uv),
-                x => x.position,
-                DynamicDraw.MakeFragmentShader<(Vector4 position, Vector2 uv)>(
-                    x => x.uv,
+            this.shader = DynamicDraw.MakeShader<Vertex, Fragment>(
+                x => (this.Camera.TransformToScreenSpace(x.Position), x.UV),
+                x => x.Position,
+                DynamicDraw.MakeFragmentShader<Fragment>(
+                    x => x.UV,
                     uv => ((int)(uv.X * 4) + (int)(uv.Y * 4)) % 2 == 0
                         ? Color.White
                         : Color.Gray));
@@ -45,7 +46,7 @@ namespace RenderLoop.Demo
                 this.Camera.Width = buffer.Width;
                 this.Camera.Height = buffer.Height;
 
-                foreach (var face in shapes)
+                foreach (var face in this.shapes)
                 {
                     DynamicDraw.DrawStrip(buffer, depthBuffer, face, BackfaceCulling.CullCounterClockwise, this.shader);
                 }
